b6884f203d7e13c611bc9204f40f023ce92bc436
shnykcc <shnykc@hotmail.com>
2020-12-26 15:25:51 +0300

    Bugları devam

--- Music/find_music.cs
+++ Music/find_music.cs
@@ -15,7 +15,10 @@ namespace Music
 {
     public partial class find_music : Form
     {
-        
+        SqlConnection bgln = new SqlConnection("Data Source =.; Initial Catalog = MusicProject; Integrated Security = True");
+        string adres = "";
+        string girilecekAdres = "";
+        string[] dosyalar;
         public find_music()
         {
             InitializeComponent();
@@ -23,18 +26,72 @@ namespace Music
 
         private void find_music_Load(object sender, EventArgs e)
         {
-            
-            
+            string dosya_yolu = "";
+            bgln.Open();
+            SqlCommand dosyaYoluOku = new SqlCommand("select Dosya_Yolu from dosyagoster where ıd=1", bgln);
+            SqlDataReader oku = dosyaYoluOku.ExecuteReader();
+            while (oku.Read())
+            {
+                dosya_yolu = oku["Dosya_Yolu"].ToString().Trim();
+            }
+            bgln.Close();
+            if (dosya_yolu == "Bos")
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                fbd.ShowDialog();
+                bgln.Open();
+                SqlCommand dosyaYoluDegis = new SqlCommand("UPDATE dosyagoster SET Dosya_Yolu='" + fbd.SelectedPath + "' WHERE ıd=1", bgln);
+                dosyaYoluDegis.ExecuteNonQuery();
+                adres = fbd.SelectedPath;
+                bgln.Close();
+            }
+            else
+            {
+                bgln.Open();
+                SqlCommand dosyaYoluCek = new SqlCommand("select Dosya_Yolu from dosyagoster where ıd=1", bgln);
+                SqlDataReader okur = dosyaYoluCek.ExecuteReader();
+                while (okur.Read())
+                {
+                    adres = okur["Dosya_Yolu"].ToString().Trim();
+                }
+                bgln.Close();
+            }
+            dosyalar = Directory.GetFiles(adres);
+            for (int i = 0; i < dosyalar.Length; i++)
+            {
+                dosyalar[i] = stringReplace(dosyalar[i].Substring(9, dosyalar[i].Length - 13));
+            }
+            foreach (var i in dosyalar)
+            {
+                Console.WriteLine(i);
+            }
+            changeSongName();
+            bgln.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (textBox1.Text != null && textBox2.Text != null && comboBox1.SelectedItem != null && comboBox2.SelectedItem != null)
+            {
+                bgln.Open();
+                SqlCommand musicGir = new SqlCommand("insert into music (Name,[Mood Name],[Tur Adi],Artist,kaydeden,yol) values('" + textBox1.Text + "','" + comboBox1.SelectedItem + "','" + comboBox2.SelectedItem + "','" + textBox2.Text + "','" + Giris.kaydeden + "','" + girilecekAdres + "')", bgln);
+                musicGir.ExecuteNonQuery();
+                bgln.Close();
+            }
+            changeSongName();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            bgln.Open();
+            SqlCommand dosyaYoluDegis = new SqlCommand("UPDATE dosyagoster SET Dosya_Yolu='" + fbd.SelectedPath + "' WHERE ıd=1", bgln);
+            dosyaYoluDegis.ExecuteNonQuery();
+            adres = fbd.SelectedPath;
+            bgln.Close();
+            dosyalar = Directory.GetFiles(adres);
+            changeSongName();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -42,6 +99,78 @@ namespace Music
             Form2 frm2 = new Form2();
             frm2.Show();
             this.Close();
+        }
+        public void changeSongName()
+        {
+            int sayac = 0, girdiSayac = 0, dosyasayac = 0;
+            bgln.Open();
+            SqlCommand sqlsayi = new SqlCommand("select count(yol) as counter from music", bgln);
+            SqlDataReader oku = sqlsayi.ExecuteReader();
+            while (oku.Read())
+            {
+                sayac = Convert.ToInt32(oku["counter"]);
+            }
+            bgln.Close();
+            string[] sqlyollarim = new string[sayac];
+            bgln.Open();
+            SqlCommand sqlyollari = new SqlCommand("select yol from music", bgln);
+            SqlDataReader oku2 = sqlyollari.ExecuteReader();
+            while (oku2.Read() && oku2["yol"] != null)
+            {
+                sqlyollarim[girdiSayac] = oku2["yol"].ToString().Trim();
+                sqlyollarim[girdiSayac] = stringReplace(sqlyollarim[girdiSayac]);
+                girdiSayac++;
+            }
+            bool sayacKontrol = false;
+            for (int i = 0; i < sayac; i++)
+            {
+                if (i <= dosyasayac)
+                {
+                    if (sqlyollarim[i] == dosyalar[dosyasayac])
+                    {
+                        Console.WriteLine("Aynı Olan bir DOsya bulundu!-------------------");
+                        dosyasayac++;
+                        i = -1;
+                        textBox1.Text = dosyalar[dosyasayac];
+                        girilecekAdres = dosyalar[dosyasayac];
+                    }
+                }
+                else
+                {
+                    sayacKontrol = true;
+                }
+                bgln.Close();
+            }
+            if (sayacKontrol == true)
+            {
+                MessageBox.Show("Bu klasördeki tüm şarkılar eklenmiş! Sonraki Açışta farklı lokasyon sectirelecek!", "Başka Şarkı Yok", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                bgln.Open();
+                SqlCommand dosyaYoluDegis = new SqlCommand("UPDATE dosyagoster SET Dosya_Yolu='Bos' WHERE ıd=1", bgln);
+                dosyaYoluDegis.ExecuteNonQuery();
+                bgln.Close();
+                sayacKontrol = false;
+            }
+
+            bgln.Close();
+        }
+        public string stringReplace(string text)
+        {
+            text = text.Replace("İ", "I");
+            text = text.Replace("ı", "i");
+            text = text.Replace("Ğ", "G");
+            text = text.Replace("ğ", "g");
+            text = text.Replace("Ö", "O");
+            text = text.Replace("ö", "o");
+            text = text.Replace("Ü", "U");
+            text = text.Replace("ü", "u");
+            text = text.Replace("Ş", "S");
+            text = text.Replace("ş", "s");
+            text = text.Replace("Ç", "C");
+            text = text.Replace("ç", "c");
+            text = text.Replace(" ", "_");
+            text = text.Replace("Ã", "U");
+            text = text.Replace("?", "_");
+            return text;
         }
     }
 }
