b0f455860ef87410c2d2a6582ab58eaed2fd1979
shnykcc <shnykc@hotmail.com>
2021-01-29 17:50:16 +0300

    Update music_save.cs

--- Music/music_save.cs
+++ Music/music_save.cs
@@ -21,8 +21,8 @@ namespace Music
         private void button1_Click(object sender, EventArgs e)
         {
             string moodKayit = "";
-            if (comboBox1.SelectedIndex == 0)
-            {
+            if (comboBox1.SelectedIndex == 0
+            
                 moodKayit = "Sad";
             }
             else if (comboBox1.SelectedIndex == 1)
